271d53e6366c132cde43964ee40d98cd74dbe2a2
takafumi.noiri <takafumi.noiri@rakuten.com>
2023-04-18 17:41:11 +0900

    initial commit

---
+++ src/main/java/com/example/todoapisample/TodoApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiSampleApplication.class, args);
+	}
+
+}
