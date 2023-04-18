4b56c1b046e31589bbf687724faa1cef38309932
takafumi.noiri <takafumi.noiri@rakuten.com>
2023-04-18 23:04:58 +0900

    I want to return ResponseEntity.ok().build()

--- src/main/java/com/example/todoapisample/controller/HealthController.java
+++ src/main/java/com/example/todoapisample/controller/HealthController.java
@@ -1,7 +1,14 @@
 package com.example.todoapisample.controller;
 
 import com.example.todoapi.controller.HealthApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
-// HealthApi
+@RestController // It is needed to add if the class has implements
 public class HealthController implements HealthApi {
+// It is easy to write with Control+O
+    @Override
+    public ResponseEntity<Void> healthGet() {
+        return ResponseEntity.ok().build();
+    }
 }
