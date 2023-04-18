0a82c606bf6753cb850d27a76feade19503e9ebd
takafumi.noiri <takafumi.noiri@rakuten.com>
2023-04-18 23:18:30 +0900

    added get annotation

--- src/main/java/com/example/todoapisample/controller/HealthController.java
+++ src/main/java/com/example/todoapisample/controller/HealthController.java
@@ -3,11 +3,13 @@ package com.example.todoapisample.controller;
 import com.example.todoapi.controller.HealthApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController // It is needed to add if the class has implements
 public class HealthController implements HealthApi {
 // It is easy to write with Control+O
     @Override
+    @GetMapping("/health")
     public ResponseEntity<Void> healthGet() {
         return ResponseEntity.ok().build();
     }
